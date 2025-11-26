630b9fbe59ed2c9e85a5953f6c6e898aec74c56e
HubTicaBB <tijana.ilic@volvocars.com>
2025-11-26 09:04:11 +0100

    Create solution

---
+++ LibraryManagement.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,84 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using LibraryManagement.Application.Interfaces;
+using LibraryManagement.Infrastructure.Data;
+using LibraryManagement.Infrastructure.Repositories;
+using LibraryManagement.Infrastructure.Services;
+
+namespace LibraryManagement.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Database
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlite(configuration.GetConnectionString("DefaultConnection")));
+
+        // Identity
+        services.AddIdentity<IdentityUser, IdentityRole>(options =>
+        {
+            options.Password.RequireDigit = true;
+            options.Password.RequireLowercase = true;
+            options.Password.RequireUppercase = true;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequiredLength = 6;
+            options.User.RequireUniqueEmail = true;
+        })
+        .AddEntityFrameworkStores<ApplicationDbContext>()
+        .AddDefaultTokenProviders();
+
+        // JWT Authentication
+        var secretKey = configuration["Jwt:SecretKey"] ?? "YourSuperSecretKeyThatIsAtLeast32CharactersLong!";
+        var issuer = configuration["Jwt:Issuer"] ?? "LibraryManagement";
+        var audience = configuration["Jwt:Audience"] ?? "LibraryManagementUsers";
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = issuer,
+                ValidAudience = audience,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey))
+            };
+            
+            // Read token from cookie if not in Authorization header
+            options.Events = new Microsoft.AspNetCore.Authentication.JwtBearer.JwtBearerEvents
+            {
+                OnMessageReceived = context =>
+                {
+                    // If no token in Authorization header, try to get it from cookie
+                    if (string.IsNullOrEmpty(context.Token))
+                    {
+                        context.Token = context.Request.Cookies["token"];
+                    }
+                    return System.Threading.Tasks.Task.CompletedTask;
+                }
+            };
+        });
+
+        // Repositories
+        services.AddScoped<IBookRepository, BookRepository>();
+        services.AddScoped<IBorrowingRepository, BorrowingRepository>();
+
+        // Services
+        services.AddScoped<IJwtTokenService, JwtTokenService>();
+
+        return services;
+    }
+}
+
